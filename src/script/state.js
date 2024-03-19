7fa477a8bbd0c03498bde2a7c312d46083931182
ditramadia <ditrarizqaamadia@gmail.com>
2024-03-19 13:06:38 +0700

    feat: add change canvas color handler

--- src/script/state.js
+++ src/script/state.js
@@ -1,6 +1,30 @@
-// Selected shape state
-let selectedShape = undefined;
+// == Selectors ===========================================================
+const canvas = document.querySelector("canvas");
 const shapeButtonDiv = document.querySelector(".shape-btn-container");
+const canvasColorInput = document.getElementById("canvas-color-input");
+
+// == State variables =====================================================
+let gl = undefined;
+let selectedShape = undefined;
+let canvasColor = [0.08, 0.08, 0.08, 1.0];
+const shapes = [];
+let isDrawing = false;
+
+// == Initialize WebGL state ==============================================
+try {
+  gl = canvas.getContext("webgl2");
+  if (!gl) {
+    throw Error("This browser does not support WebGL 2.");
+  }
+
+  // Clear the color and depth buffer
+  gl.clearColor(...canvasColor);
+  gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+} catch (error) {
+  showError(`Initialize canvas failed - ${error}`);
+}
+
+// == Select shape event handler ==========================================
 for (let i = 0; i < shapeButtonDiv.children.length; i++) {
   shapeButtonDiv.children[i].addEventListener("click", () => {
     if (selectedShape === shapeButtonDiv.children[i].id) {
@@ -17,14 +41,14 @@ for (let i = 0; i < shapeButtonDiv.children.length; i++) {
   });
 }
 
-// Shapes exist in the canvas
-const shapes = [];
+// == Change canvas background color handler ==============================
+canvasColorInput.addEventListener("input", () => {
+  canvasColor = hexToRGBA(canvasColorInput.value);
+  gl.clearColor(...canvasColor);
+  gl.clear(gl.COLOR_BUFFER_BIT);
+});
 
-// Drawing state
-let isDrawing = false;
-
-const canvas = document.querySelector("canvas");
-
+// == Drawing state handler ===============================================
 canvas.addEventListener("mousedown", (e) => {
   isDrawing = true;
   showLog("User started drawing");
